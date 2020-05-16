5d7fef01f37a2c166a74dfc0763398269370be6f
ichirou2910 <ichiroukeita2910@gmail.com>
2020-05-16 10:02:19 +0700

    Polybar: Add arch update function when click

--- .config/polybar/updates-arch.sh
+++ .config/polybar/updates-arch.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
+case "$1" in
+    --info)
+        if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+            updates_arch=0
+        fi
 
-if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
-    updates_aur=0
-fi
+        if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+            updates_aur=0
+        fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+        updates=$(("$updates_arch" + "$updates_aur"))
 
-if [ "$updates" -gt 0 ]; then
-    echo "$updates"
-else
-    echo ""
-fi
+        if [ "$updates" -gt 0 ]; then
+            echo "$updates"
+        else
+            echo ""
+        fi
+        ;;
+    --update)
+        if pgrep -c $TERMINAL >/dev/null 2>&1; then
+            tmux attach || tmux new-session
+            tmux new-window -n Update yay -Syu
+        else
+            $TERMINAL -e tmux attach || tmux new-session
+            tmux new-window -n Update yay -Syu
+        fi
+        ;;
+esac
