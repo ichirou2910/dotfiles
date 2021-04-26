21cbbe04de4efd77ba4e0ea654b3f4b7d1e2b4d0
Ichirou2910 <ichiroukeita2910@gmail.com>
2021-04-26 09:07:25 +0700

    Powermenu icons and choices

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 source $HOME/.owl4ce_var
 
-rofi_command="rofi -theme themes/sidebar/five.rasi"
+rofi_command="rofi -theme themes/sidebar/three.rasi"
 
 # Options
-shutdown="⏻"
-reboot=""
-lock=""
-logout=""
+shutdown=""
+reboot=""
+logout=""
 
 # Variable passed to rofi
-options="$shutdown\n$reboot\n$lock\n$logout"
+options="$shutdown\n$reboot\n$logout"
 
-chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0)"
 case $chosen in
     $shutdown)  $ROFI_DIR/scripts/promptmenu.sh --yes-command "poweroff" --query "     Poweroff?"
     ;;
     $reboot)    $ROFI_DIR/scripts/promptmenu.sh --yes-command "reboot" --query "      Reboot?"
-    ;;
-    $lock)      $DEFAPPS_EXEC lockscreen
     ;;
     $logout)    $ROFI_DIR/scripts/promptmenu.sh --yes-command "pkill -KILL -u $(whoami)" --query "      Logout?"
     ;;
