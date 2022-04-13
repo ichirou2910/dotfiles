5cca83c2f9fa83d39d52492453bd13d3955d183c
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-04-13 16:56:24 +0700

    toggle panel

--- .scripts/toggle-panel.sh
+++ .scripts/toggle-panel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sed -i -r 's/(autohide\ \=\ )0/\11/;t;s/(autohide\ \=\ )1/\10/' ~/.config/tint2/tint2rc;killall -SIGUSR1 tint2
-sed -i -r 's/(<top>)0/\145/;t;s/(<top>)45/\10/' ~/.config/openbox/rc.xml;openbox --reconfigure
+sed -i -r 's/(autohide\ \=\ )0/\11/;t;s/(autohide\ \=\ )1/\10/' ~/.config/tint2/tint2rc
+killall -SIGUSR1 tint2
+sed -i -r 's/(<top>)0/\145/;t;s/(<top>)45/\10/' ~/.config/openbox/rc.xml
+openbox --reconfigure
+sed -i -r 's/(top\=\")50/\15/;t;s/(top\=\")5/\150/' ~/.config/rtile/rtile.xml
