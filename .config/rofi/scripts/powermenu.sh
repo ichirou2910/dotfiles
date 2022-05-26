6f2267b9c3dc7674e13b646a87cbbd6f747ab9b6
Ichirou2910 <ichiroukeita2910@gmail.com>
2022-05-26 08:58:09 +0700

    rofi

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 source $HOME/.ik_var
 
-rofi_command="rofi -theme themes/sidebar/three.rasi"
+rofi_command="rofi -theme themes/sidebar/four.rasi"
 
 # Options
 shutdown=""
 reboot=""
 logout=""
+lock=""
 
 # Variable passed to rofi
-options="$shutdown\n$reboot\n$logout"
+options="$shutdown\n$reboot\n$lock\n$logout"
 
 chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0)"
 case $chosen in
@@ -21,5 +22,8 @@ $reboot)
 	;;
 $logout)
 	$ROFI_DIR/scripts/promptmenu.sh --yes-command "pkill -KILL -u $(whoami)" --query "      Logout?"
+	;;
+$lock)
+	$ROFI_DIR/scripts/promptmenu.sh --yes-command "slock" --query "      Lock?"
 	;;
 esac
