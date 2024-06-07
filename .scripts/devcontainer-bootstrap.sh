0a2ac1b51c1e8e1b207bf7524636d111b163fc9c
ichirou2910 <ichiroukeita2910@gmail.com>
2024-06-07 22:58:06 +0700

    feat: add devcontainer install script

---
+++ .scripts/devcontainer-bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script install dotfiles into devcontainers
+
+export XDG_CONFIG_HOME="$HOME"/.config
+mkdir -p "$XDG_CONFIG_HOME"
+
+ln -sf "$PWD/.config/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/.config/zsh" "$XDG_CONFIG_HOME"/zsh
