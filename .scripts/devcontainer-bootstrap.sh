685727ce5b760add714fe31d69adc9bdd0fd872b
ichirou2910 <ichiroukeita2910@gmail.com>
2024-06-07 23:45:20 +0700

    fix(devcontainer): install script

--- .scripts/devcontainer-bootstrap.sh
+++ .scripts/devcontainer-bootstrap.sh
@@ -3,7 +3,11 @@
 # This script install dotfiles into devcontainers
 
 export XDG_CONFIG_HOME="$HOME"/.config
+export XDG_CACHE_HOME="$HOME"/.cache
 mkdir -p "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_CACHE_HOME"
 
-ln -sf "$PWD/.config/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/.config/zsh" "$XDG_CONFIG_HOME"/zsh
+ln -sf "$PWD/.config/nvim" "$XDG_CONFIG_HOME/nvim"
+ln -sf "$PWD/.config/zsh" "$XDG_CONFIG_HOME/zsh"
+
+cp -ru "$PWD/.cache/*" "$XDG_CACHE_HOME/"
