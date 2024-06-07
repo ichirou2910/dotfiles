ab03184aeb556a4f0a5b24658bb8fa03cc097c7d
ichirou2910 <ichiroukeita2910@gmail.com>
2024-06-07 19:03:34 +0700

    chore: remove all unused config (part 5)

--- .scripts/notify/stretch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DISPLAY=:0
-PID=$(pgrep openbox)
-DBUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"$PID"/environ | cut -d= -f2-)
-export DBUS_SESSION_BUS_ADDRESS=$DBUS_ADDRESS
-
-/usr/bin/notify-send.sh "Quick Event" "Hey, stretch yourself a bit." \
-	--hint string:image-path:"file:///home/ichirou2910/.icons/clock.png"
--- .scripts/toggle-panel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i -r 's/(autohide\ \=\ )0/\11/;t;s/(autohide\ \=\ )1/\10/' ~/.config/tint2/tint2rc
-killall -SIGUSR1 tint2
-sed -i -r 's/(<bottom>)0/\145/;t;s/(<bottom>)45/\10/' ~/.config/openbox/rc.xml
-openbox --reconfigure
-sed -i -r 's/(bottom\=\")50\"/\15\"/;t;s/(bottom\=\")5\"/\150\"/' ~/.config/rtile/rtile.xml
