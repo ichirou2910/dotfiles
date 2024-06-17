090f2eeee1d9e8ec165d7507900735948e1e7a49
ichirou2910 <ichiroukeita2910@gmail.com>
2024-06-17 10:01:16 +0700

    fix(nvim): missing files

---
+++ .config/nvim/lua/plugins/remote-nvim.lua
@@ -0,0 +1,19 @@
+return {
+    -- {
+    --     "amitds1997/remote-nvim.nvim",
+    --     version = "*", -- Pin to GitHub releases
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim", -- For standard functions
+    --         "MunifTanjim/nui.nvim", -- To build the plugin UI
+    --         "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    --     },
+    --     config = {
+    --         devpod = {
+    --             dotfiles = {
+    --                 path = "https://github.com/ichirou2910/dotfiles",
+    --                 install_script = ".scripts/devcontainer-bootstrap.sh",
+    --             },
+    --         },
+    --     },
+    -- },
+}
