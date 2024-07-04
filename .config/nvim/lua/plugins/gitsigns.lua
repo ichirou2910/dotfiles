650074954af72433824beadf0cca6df34ca3a273
ichirou2910 <ichiroukeita2910@gmail.com>
2024-07-05 00:30:28 +0700

    fix(nvim/gitsigns): update new config

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,33 +5,6 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             require("gitsigns").setup({
-                signs = {
-                    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-                    change = {
-                        hl = "GitSignsChange",
-                        text = "│",
-                        numhl = "GitSignsChangeNr",
-                        linehl = "GitSignsChangeLn",
-                    },
-                    delete = {
-                        hl = "GitSignsDelete",
-                        text = "_",
-                        numhl = "GitSignsDeleteNr",
-                        linehl = "GitSignsDeleteLn",
-                    },
-                    topdelete = {
-                        hl = "GitSignsDelete",
-                        text = "‾",
-                        numhl = "GitSignsDeleteNr",
-                        linehl = "GitSignsDeleteLn",
-                    },
-                    changedelete = {
-                        hl = "GitSignsChange",
-                        text = "~",
-                        numhl = "GitSignsChangeNr",
-                        linehl = "GitSignsChangeLn",
-                    },
-                },
                 on_attach = function(bufnr)
                     local gs = package.loaded.gitsigns
 
@@ -90,21 +63,6 @@ return {
                     interval = 1000,
                     follow_files = true,
                 },
-                attach_to_untracked = true,
-                current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame_opts = {
-                    virt_text = true,
-                    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-                    delay = 1000,
-                    ignore_whitespace = false,
-                },
-                current_line_blame_formatter_opts = {
-                    relative_time = false,
-                },
-                sign_priority = 6,
-                update_debounce = 100,
-                status_formatter = nil, -- Use default
-                max_file_length = 40000,
                 preview_config = {
                     -- Options passed to nvim_open_win
                     border = "rounded",
@@ -112,9 +70,6 @@ return {
                     relative = "cursor",
                     row = 0,
                     col = 1,
-                },
-                yadm = {
-                    enable = false,
                 },
             })
         end,
