cd6912df7825783a5881b51660f0ebbf952e63c7
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-23 20:28:30 +0700

    fix(dev): bump azuredatastudio

--- .config/nixos/home/ichirou2910/features/dev/default.nix
+++ .config/nixos/home/ichirou2910/features/dev/default.nix
@@ -4,7 +4,7 @@ let
     src = pkgs.fetchurl {
       name = "azuredatastudio-1.48.1.tar.gz";
       url = "https://azuredatastudio-update.azurewebsites.net/1.48.1/linux-x64/stable";
-      sha256 = "sha256-bshkpcs1Ob7gdBWerjEWLJ/FUBjcwYb6Mv+cIIaxDWw=";
+      sha256 = "sha256-0LCrRUTTe8UEDgtGLvxVQL8pA5dwA6SvZEZSDILr7jo=";
     };
   });
 
