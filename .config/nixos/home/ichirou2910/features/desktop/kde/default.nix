f069261f3eb2b6f900fed57170cfbda1c9b69608
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-24 01:10:07 +0700

    feat(kde): add theme packages

--- .config/nixos/home/ichirou2910/features/desktop/kde/default.nix
+++ .config/nixos/home/ichirou2910/features/desktop/kde/default.nix
@@ -1,10 +1,44 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let
+  nothing-theme = pkgs.stdenv.mkDerivation {
+    name = "nothing-theme";
+    version = "latest";
+    src = pkgs.fetchFromGitLab {
+      owner = "jomada";
+      repo = "nothing";
+      rev = "853d548dc4b0d1a0bcfe78c3893bfbf4231fd073";
+      hash = "sha256-Qw5H0yzirIde9JuqiBa4pcJz0QAnsZk/17U+Mgn1l7k=";
+    };
+
+    buildPhase = ''
+      mkdir -p aurorae/themes
+      mv aurorae/Nothing aurorae/themes/Nothing
+
+      mkdir -p plasma/desktoptheme
+      mv Nothing plasma/desktoptheme/Nothing
+
+      mv look-and-feel plasma/look-and-feel
+    '';
+
+    installPhase = ''
+      install -d $out/share
+      cp -r aurorae color-schemes plasma wallpapers $out/share
+    '';
+  };
+in
+{
   imports = [
     ../common
     ./fonts.nix
     ./hotkeys.nix
     ./panels.nix
     ./workspace.nix
+  ];
+
+  # Themes
+  home.packages = with pkgs; [
+    papirus-icon-theme
+    nothing-theme
   ];
 
   xdg.portal.extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
