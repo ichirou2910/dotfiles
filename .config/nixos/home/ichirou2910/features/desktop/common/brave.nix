1aca7e6dda9b1b8f8072a1f7ed8844ccf323e927
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-24 16:05:24 +0700

    fix(brave): proper custom desktop file

--- .config/nixos/home/ichirou2910/features/desktop/common/brave.nix
+++ .config/nixos/home/ichirou2910/features/desktop/common/brave.nix
@@ -9,9 +9,31 @@
 
   xdg.desktopEntries = {
     "brave-browser" = {
-      exec = "${pkgs.brave}/bin/brave --enable-features=UseOzonePlatform --enable-wayland-ime";
-      icon = "brave-browser";
       name = "Brave Web Browser";
+      genericName = "Web Browser";
+      comment = "Access the Internet";
+      exec = "${pkgs.brave}/bin/brave --enable-features=UseOzonePlatform --enable-wayland-ime %U";
+      icon = "brave-browser";
+      terminal = false;
+      startupNotify = true;
+      type = "Application";
+      categories = [ "Network" "WebBrowser" ];
+      mimeType = [
+        "application/pdf" "application/rdf+xml" "application/rss+xml" "application/xhtml+xml"
+        "application/xhtml_xml" "application/xml" "image/gif" "image/jpeg" "image/png"
+        "image/webp" "text/html" "text/xml" "x-scheme-handler/http" "x-scheme-handler/https"
+      ];
+      actions = {
+        "new-window" = {
+          name = "New Window";
+          exec = "${pkgs.brave}/bin/brave";
+        };
+
+        "new-private-window" = {
+          name = "New Incognito Window";
+          exec = "${pkgs.brave}/bin/brave --incognito";
+        };
+      };
     };
   };
 }
