63e1438f735c242504312771623fe4fdb15cd0e0
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-26 12:43:33 +0700

    feat(nixos/nvim): add tools

--- .config/nixos/home/ichirou2910/features/nvim.nix
+++ .config/nixos/home/ichirou2910/features/nvim.nix
@@ -10,16 +10,20 @@
   };
 
   home.packages = with pkgs; [
+    # lsp
     roslyn-ls
     phpactor
     lua-language-server
     nodePackages_latest.typescript-language-server
 
+    # debugger
     netcoredbg
 
+    # formater/linter
     csharpier
     stylua
     shfmt
     eslint_d
+    prettierd
   ];
 }
