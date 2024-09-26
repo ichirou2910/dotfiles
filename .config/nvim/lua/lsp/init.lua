36d75a18524b025ab887af1eac5ded637850936a
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-26 12:42:58 +0700

    fix(nvim): remove mason dependencies

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -74,13 +74,6 @@ local mason_servers = {
             },
         },
     },
-    csharp_ls = {
-        on_attach = lsp_utils.lsp_attach,
-        capabilities = lsp_utils.get_capabilities(),
-        handlers = {
-            ["textDocument/definition"] = require("csharpls_extended").handler,
-        },
-    },
     phpactor = {
         on_attach = lsp_utils.lsp_attach,
         capabilities = lsp_utils.get_capabilities(),
@@ -90,19 +83,6 @@ local mason_servers = {
 for k, v in pairs(mason_servers) do
     lspconfig[k].setup(v)
 end
-
--- local mason_server_names = vim.tbl_keys(mason_servers)
--- mason.setup()
--- mason_lspconfig.setup_handlers({
---     function(server_name)
---         if vim.tbl_contains(mason_server_names, server_name) then
---             lspconfig[server_name].setup(mason_servers[server_name])
---         end
---     end,
--- })
--- mason_lspconfig.setup({
---     ensure_installed = mason_server_names,
--- })
 
 -- tsserver
 local function lsp_tsserver()
@@ -116,6 +96,39 @@ local function lsp_tsserver()
     })
 end
 lsp_tsserver()
+
+-- roslyn
+local function lsp_roslyn()
+    require("roslyn").setup({
+        exe = {
+            "Microsoft.CodeAnalysis.LanguageServer",
+        },
+        config = {
+            on_attach = lsp_utils.lsp_attach,
+            capabilities = lsp_utils.get_capabilities(),
+            settings = {
+                ["csharp|inlay_hints"] = {
+                    csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                    csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                    csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                    csharp_enable_inlay_hints_for_types = true,
+                    dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                    dotnet_enable_inlay_hints_for_literal_parameters = true,
+                    dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                    dotnet_enable_inlay_hints_for_other_parameters = true,
+                    dotnet_enable_inlay_hints_for_parameters = true,
+                    dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                    dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                    dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                },
+                ["csharp|code_lens"] = {
+                    dotnet_enable_references_code_lens = true,
+                },
+            },
+        },
+    })
+end
+lsp_roslyn()
 
 -- null-ls
 require("lsp.null-ls").setup()
