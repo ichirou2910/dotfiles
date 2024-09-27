fc64911250e97994cff78fefd9aedf25a555b6d6
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-27 17:24:59 +0700

    feat(kde): 2nd monitor panel

--- .config/nixos/home/ichirou2910/features/desktop/kde/panels.nix
+++ .config/nixos/home/ichirou2910/features/desktop/kde/panels.nix
@@ -2,6 +2,7 @@
   programs.plasma.panels = [
     # main screen
     {
+      screen = 0;
       floating = true;
       height = 45;
       hiding = "none";
@@ -28,6 +29,58 @@
         }
         "org.kde.plasma.marginsseparator"
         "org.kde.plasma.systemtray"
+        {
+          digitalClock = {
+            date.format = { 
+              custom = "ddd, MMM d, yyyy";
+            };
+          };
+        }
+      ];
+    }
+    # 2nd screen
+    {
+      screen = 1;
+      floating = true;
+      height = 45;
+      hiding = "none";
+      lengthMode = "fill";
+      location = "bottom";
+      widgets = [
+        {
+          kickoff = {
+            icon = "distributor-logo-nixos";
+          };
+        }
+        {
+          iconTasks = {
+            behavior = {
+              showTasks = {
+                onlyInCurrentScreen = true;
+                onlyInCurrentDesktop = true;
+                onlyInCurrentActivity = true;
+                onlyMinimized = false;
+              };
+            };
+            launchers = [ ];
+          };
+        }
+        {
+          systemMonitor = {
+            title = "Total CPU Use";
+            showTitle = true;
+            showLegend = true;
+            displayStyle = "org.kde.ksysguard.piechart";
+            sensors = [
+              {
+                name = "cpu/all/usage";
+                color = "212,21,21";
+                label = "CPU %";
+              }
+            ];
+          };
+        }
+        "org.kde.plasma.marginsseparator"
         {
           digitalClock = {
             date.format = { 
