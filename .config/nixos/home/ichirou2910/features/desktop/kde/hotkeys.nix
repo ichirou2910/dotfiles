101766b932dd49e61373cf88588e67359b730301
ichirou2910 <ichiroukeita2910@gmail.com>
2024-09-27 17:27:43 +0700

    fix(kde): shortcuts

--- .config/nixos/home/ichirou2910/features/desktop/kde/hotkeys.nix
+++ .config/nixos/home/ichirou2910/features/desktop/kde/hotkeys.nix
@@ -1,13 +1,16 @@
 {
   programs.plasma.shortcuts = {
     # Set Meta+Q to Window Close
-    "plasmashell"."manage activities" = "\\, Meta+Q\\, ,Meta+Q,Show Activity Switcher";
-    "kwin"."Window Close" = ["Alt+F4" "Meta+Q,Alt+F4,Close Window"];
+    "plasmashell"."manage activities" = "none,Meta+Q,Show Activity Switcher";
+    "kwin"."Window Close" = ["Meta+Q" "Alt+F4,Alt+F4,Close Window"];
 
     # Set Meta+W to Brave shortcut
-    "kwin"."Overview" = "\\, Meta+W\\, ,Meta+W,Toggle Overview";
+    "kwin"."Overview" = ",Meta+W,Toggle Overview";
     "services/brave-browser.desktop"."_launch" = "Meta+W";
 
     "services/kitty.desktop"."_launch" = "Meta+Return";
+
+    "kwin"."Window Maximize" = ["Meta+M" "Meta+PgDown,Meta+PgDown,Minimize Window"];
+    "kwin"."Window Minimize" = ["Meta+Shift+M" "Meta+PgDown,Meta+PgDown,Minimize Window"];
   };
 }
