64daa90a49b916df1115e687dde1b98459f9c3c3
ichirou2910 <ichiroukeita2910@gmail.com>
2024-10-04 00:45:12 +0700

    fix(nvim): replace indent-blankline with hlchunk

---
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "shellRaining/hlchunk.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("hlchunk").setup({
+                chunk = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+}
