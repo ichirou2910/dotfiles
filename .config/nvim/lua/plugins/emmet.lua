5e32d317ff0fa6efef24f3e84c2390a173b598f6
ichirou2910 <ichiroukeita2910@gmail.com>
2024-10-04 01:08:10 +0700

    chore(nvim): remove unused plugins

--- .config/nvim/lua/plugins/emmet.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "mattn/emmet-vim",
-        ft = { "html", "typescriptreact", "javascriptreact" },
-        config = function()
-            vim.cmd([[ let g:user_emmet_leader_key = ',' ]])
-        end,
-    },
-}
