06e1f24465f3a5707f51653c34ba063cf7c08dc0
ichirou2910 <ichiroukeita2910@gmail.com>
2025-01-20 10:03:33 +0700

    refactor: remove obsidian & cleanup markdown

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -14,12 +14,15 @@ return {
                 checkboxes = presets.checkboxes.nerd,
                 headings = presets.headings.marker,
                 horizontal_rules = presets.horizontal_rules.thin,
+
+                hybrid_modes = nil,
             })
         end,
 
         keys = {
             { "<leader>mh", ft = "markdown", mode = "n", "<cmd>Markview hybridToggle<cr>", desc = "Toggle hybrid" },
             { "<leader>ms", ft = "markdown", mode = "n", "<cmd>Markview splitToggle<cr>", desc = "Toggle split" },
+            { "<leader>mt", ft = "markdown", mode = "n", "<cmd>Markview toggle<cr>", desc = "Toggle" },
         },
     },
 }
