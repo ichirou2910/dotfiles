68963f438a5c6928020a4cbe0a4e96a33b1afb92
ichirou2910 <ichiroukeita2910@gmail.com>
2025-01-22 10:07:36 +0700

    fix(nvim): remove neotest-dotnet

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -5,13 +5,11 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
-            "Issafalcon/neotest-dotnet",
             "rouge8/neotest-rust",
         },
         config = function()
             require("neotest").setup({
                 adapters = {
-                    require("neotest-dotnet"),
                     require("neotest-rust"),
                 },
             })
