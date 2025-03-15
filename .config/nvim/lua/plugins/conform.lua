b62d859d320c58973e8dacf5f53e97ff689dc384
ichirou2910 <ichiroukeita2910@gmail.com>
2025-03-16 00:43:22 +0700

    feat(nvim/nix): add alejandra lsp

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -37,6 +37,7 @@ return {
                 javascript = { "prettierd", "prettier", stop_after_first = true },
                 json = { "jq" },
                 sh = { "shfmt" },
+                nix = { "alejandra" },
             },
         },
     },
